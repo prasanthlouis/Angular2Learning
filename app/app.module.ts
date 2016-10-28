2a8c33f8fd1e11a3985ead5202d29ceb6d44f10f
Prasanth Louis <prasanthlouis21@gmail.com>
2016-10-27 21:52:16 -0400

    Next

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent }  from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
+import { Http } from '@angular/http';
  
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, Http ],
   declarations: [ AppComponent,
   ProductListComponent,
   ProductFilterPipe,
